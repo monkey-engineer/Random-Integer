1776b9b90c03aca84b041dc1b0f83f5d9cc3bc64
monkey-engineer <wallarookiller@yahoo.com>
2016-12-27 22:47:44 -0700

    Update random.js

--- random.js
+++ random.js
@@ -1,8 +1,55 @@
-var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
 
-function randomWholeNum() {
+<script LANGUAGE="JavaScript">
 
-  // Only change code below this line.
+<!-- hide from non-scriptable browsers
 
-  return Math.random();
+
+
+function chooseRandom(form) {
+
+var m = parseInt(form.lower.value);
+
+var n = parseInt(form.upper.value) - m;
+
+
+
+// alert("m is is " + m + " n is " + n);
+
+
+
+var output = Math.round(Math.random() * n) + m;
+
+form.result.value = output;
+
+
+
+
+
 }
+
+
+
+
+
+// Clear a form
+
+function clearForm(form) {
+
+	form.upper.value="";
+
+	form.lower.value="";
+
+	form.upper.focus();
+
+    form.upper.select();
+
+}
+
+// end hiding -->
+
+</script>
+
+
+
+
+
